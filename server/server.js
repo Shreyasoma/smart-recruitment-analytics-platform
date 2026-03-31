5d91650091b4d1c143ca3267907008562af67009
Shreya <shreyaforany@gmail.com>
2026-03-31 15:02:40 +0530

    feat: add database schema with users, candidates, candidate_skills tables

--- server/server.js
+++ server/server.js
@@ -4,6 +4,8 @@ require('dotenv').config();
 const db = require('./config/db');
 const app = express();
 app.use(cors());
+app.use(express.json());
+app.use('/api/auth', require('./routes/auth'));
 
 const PORT = process.env.PORT || 3000;
 
